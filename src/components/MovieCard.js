e242e0a109d30255eb7763ba3d25bc5af6468d1e
Sergio Andres Llanos Garcia <sallanos@bancolombia.com.co>
2019-05-23 19:46:07 -0500

    Feat: Add the images to the movies

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -40,7 +40,7 @@ class MovieCard extends React.Component {
   }
   
   render() {
-    const { id, title, year, image, overview, deleteMovie } = this.props
+    const { id, title, year, poster_path, overview, deleteMovie } = this.props
     return <div className='movie-container'>   
         <div className='favourite-container'>
             <button onClick={this.handleCheck} className='favourite-button'>
@@ -53,7 +53,7 @@ class MovieCard extends React.Component {
       <h1>{title}</h1>
       <div>{year}</div>
       <div className='overview-container'>
-        <img className='overview-img' src={image} alt='Movie' />
+        <img className='overview-img' src={`https://image.tmdb.org/t/p/w1280${poster_path}`} alt='Movie' />
         <p>{overview}</p>
       </div>
     </div>
