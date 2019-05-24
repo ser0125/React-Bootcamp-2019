a03e7382373b0e283507cd04c1a97a6d1e54228d
Sergio Andres Llanos Garcia <sallanos@bancolombia.com.co>
2019-05-23 19:40:17 -0500

    Feat: my 5 movies

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import moviesData from '../data/movies.json'
 import MovieCard from '../components/MovieCard'
 
+const API_KEY = 'd8862dd490c5a0eee2e11969564ca8e7';
+
 class Home extends React.Component {
   state = {
-   ...moviesData,
+    moviesData: ''
   }
 
   deleteMovie = (movieId) => {
@@ -16,15 +18,30 @@ class Home extends React.Component {
     })
   }
 
+  componentDidMount () {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({
+        moviesData: data.results
+      })
+    })
+  }
+
 
     render() {
-        const { movies } = this.state
-        return <div>
-        <h1 className='main-title'>Movie App</h1>
-        <div className='content'>
-          {movies.map((movie) => <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />)}
+        let  movies = this.state.moviesData;
+        if(movies){
+          console.log(this.state.movies);
+          return <div>
+          <h1 className='main-title'>Movie App</h1>
+          <div className='content'>
+            {movies.map((movie, index) => index < 5 ? <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />: null)}
+          </div>
         </div>
-      </div>
+        }
+        return null;
+        
     }
 }
 
