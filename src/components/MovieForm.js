212f3263d45737be0586c3ecabdd79a5db606850
Sergio Andres Llanos Garcia <sallanos@bancolombia.com.co>
2019-05-28 18:50:56 -0500

    feat: add movieform in to the proyect

---
+++ src/components/MovieForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class MovieForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      id: '',
+      title: '',
+      year: '',
+      image: '',
+      genre: '',
+      overview: ''
+    };
+    this.yearRef = React.createRef();
+  }
+
+  handleChange = (e) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+  }
+
+  componentDidMount () {
+    console.log(this.yearRef.current.focus());
+  }
+
+  render () {
+    return <form className='form' onSubmit={this.handleSubmit}>
+      <input id='movieID' value={this.state.id} name='id' placeholder='id' onChange={this.handleChange} />
+      <input value={this.state.title} name='title' placeholder='title' onChange={this.handleChange} />
+      <input ref={this.yearRef} value={this.state.year} name='year' placeholder='year' onChange={this.handleChange} />
+      <input value={this.state.image} name='image' placeholder='image' onChange={this.handleChange} />
+      <input value={this.state.genre} name='genre' placeholder='genre' onChange={this.handleChange} />
+      <input value={this.state.overview} name='overview' placeholder='overview' onChange={this.handleChange} />
+      <input type="submit" value="Save"/>
+    </form>
+
+  }
+}
+
+export default MovieForm;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import moviesData from '../data/movies.json'
 import MovieCard from '../components/MovieCard'
+import MovieForm from '../components/MovieForm.js';
 
 const API_KEY = 'd8862dd490c5a0eee2e11969564ca8e7';
 
 class Home extends React.Component {
   state = {
     moviesData: ''
+  }
+
+  addMovie = (movie) => {
+    const movies = this.state.moviesData;
+    movies.push(movie);
+    this.setState({ ...movies})
   }
 
   deleteMovie = (movieId) => {
@@ -34,6 +40,7 @@ class Home extends React.Component {
         if(movies){
           return <div>
           <h1 className='main-title'>Movie App</h1>
+          <MovieForm onSubmit={this.addMovie}/>
           <div className='content'>
             {movies.map((movie, index) => index < 5 ? <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />: null)}
           </div>
